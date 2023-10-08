a037ecd3267459d0bb180de2a0d83c41a2b7648b
Huy Long Nguyen <huylongnguyen0605@gmail.com>
2023-10-08 02:04:36 -0700

    fixed vertical movement, added jump, added gravity

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,29 +1,24 @@
 extends KinematicBody2D
+#declare variable
+var speed = 200
+var jump_speed = -800
+var gravity = 4000
+var velocity = Vector2.ZERO
+var dashSpeed = 10
 
-
-# Variables
-var maxSpeed = 200
-
+#movements
 func _physics_process(delta):
-	var velocity = Vector2.ZERO
+	velocity.x = 0
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += speed
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= speed
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
-	if Input.action_press("ui_right"):
-		velocity.x += 1
-	if Input.action_press("ui_left"):
-		velocity.x -= 1
-	if Input.action_press("ui_up"):
-		velocity.y += 1
-	if Input.action_press("ui_down"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * maxSpeed
+	if Input.is_action_just_pressed("ui_jump"):
+		if is_on_floor():
+			velocity.y = jump_speed
+			
+
 		
-	move_and_slide(velocity)
-
-	
-	#Jump
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
