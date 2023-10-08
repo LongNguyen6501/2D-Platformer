94ff09498303fe3a83f86047eb0d920aba37334b
Huy Long Nguyen <huylongnguyen0605@gmail.com>
2023-10-08 02:08:46 -0700

    added comments on player movements

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,17 +8,18 @@ var dashSpeed = 10
 
 #movements
 func _physics_process(delta):
+	#player idle velocity =0
 	velocity.x = 0
+	#go left and right
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += speed
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= speed
+	#player gravity code
 	velocity.y += gravity * delta
+	#allows player movements
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
+	#jump mechaninism
 	if Input.is_action_just_pressed("ui_jump"):
 		if is_on_floor():
 			velocity.y = jump_speed
-			
-
-		
