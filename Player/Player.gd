d8a5971a3ca676aeea71c9800dfcb32fa7885d23
Huy Long Nguyen <huylongnguyen0605@gmail.com>
2023-10-20 05:17:03 -0700

    smoothened jumping, applied dash + attack animation. bugs: dash's and attack's animations only run while holding corresponding buttons, as soon as the button is released, they will get overwritten by idle or run animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,46 +10,57 @@ var attacking = -1
 var dashing = -1
 onready var anim = get_node("AnimationPlayer")
 func _physics_process(delta):
+	var currentAnim = anim.get_current_animation()
 	#player gravity code
-	if not is_on_floor():
+	if not is_on_floor() and currentAnim != "Run":
 		velocity.y += gravity * delta
+
 	#player idle velocity =0
 	velocity.x = 0
-	attacking = -1
-	dashing = -1
-		#jump mechaninism
-	if Input.is_action_just_pressed("ui_jump"):
-		if is_on_floor():
+
+	#jump
+	if is_on_floor() or currentAnim == "Run":
+		if Input.is_action_just_pressed("ui_jump"):
 			velocity.y = jump_speed
 			anim.play("Jump")
-	elif Input.is_action_just_pressed("ui_attack"):
+
+	#attack
+	if Input.is_action_just_pressed("ui_attack"):
 		attacking = 1
 		anim.play("Attack1")
-	#go left and right
-	elif Input.is_action_pressed("ui_right"):
-		face = 1
-		velocity.x += speed * face
-		if is_on_floor() and attacking != 1 and dashing != 1:
-			anim.play("Run")
-	elif Input.is_action_pressed("ui_left"):
-		face = -1
-		velocity.x += speed * face
-		if is_on_floor() and attacking != 1 and dashing != 1:
-			anim.play("Run")
-
-	#dash mechanism
+	elif Input.is_action_just_released("ui_attack"):
+		attacking = -1
+	#dash
 	elif Input.is_action_just_pressed("ui_dash"):
 		dashing = 1
 		velocity.x += dash_speed * face
 		anim.play("Dash")
-	#attack mechanism
+	elif Input.is_action_just_released("ui_dash"):
+		dashing = -1
 
-	elif is_on_floor() and attacking != 1 and dashing != 1:
+	#go left and right
+	elif Input.is_action_pressed("ui_right"):
+		face = 1
+		velocity.x += speed * face
+		if is_on_floor() and velocity.x != 0 and attacking == -1 and dashing == -1:
+			anim.play("Run")
+	elif Input.is_action_pressed("ui_left"):
+		face = -1
+		velocity.x += speed * face
+		if is_on_floor() and velocity.x != 0 and attacking == -1 and dashing == -1:
+			anim.play("Run")
+	#idle
+	elif velocity.x == 0 and velocity.y ==0 and attacking == -1 and dashing == -1:
 		anim.play("Idle")
+
+	#Flip sprite
 	if face < 0:
 		$AnimatedSprite.flip_h = true
 	elif face > 0:
-		$AnimatedSprite.flip_h = false 
+		$AnimatedSprite.flip_h = false
+
+#debug printing 
+	print(velocity.x, ",", velocity.y)
 
 	#allows player movements
 	velocity = move_and_slide(velocity, Vector2.UP)
