3b54fd42e623f96c82d31c6d004b0c8fe12cddc1
Huy Long Nguyen <huylongnguyen0605@gmail.com>
2023-10-20 02:53:45 -0700

    applied jump animation, flip sprite when changing direction

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,39 +6,50 @@ var gravity = 4000
 var velocity = Vector2.ZERO
 var dash_speed = 6000
 var face = 1
+var attacking = -1
+var dashing = -1
 onready var anim = get_node("AnimationPlayer")
-func _ready():
-	anim.play("Idle")
 func _physics_process(delta):
 	#player gravity code
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	
+	#player idle velocity =0
+	velocity.x = 0
+	attacking = -1
+	dashing = -1
 		#jump mechaninism
 	if Input.is_action_just_pressed("ui_jump"):
 		if is_on_floor():
 			velocity.y = jump_speed
 			anim.play("Jump")
-		if velocity.y > 0:
-			anim.play("Fall")
-	#player idle velocity =0
-	velocity.x = 0
+	elif Input.is_action_just_pressed("ui_attack"):
+		attacking = 1
+		anim.play("Attack1")
 	#go left and right
-	if Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right"):
 		face = 1
 		velocity.x += speed * face
-		anim.play("Run")
+		if is_on_floor() and attacking != 1 and dashing != 1:
+			anim.play("Run")
 	elif Input.is_action_pressed("ui_left"):
 		face = -1
 		velocity.x += speed * face
-		anim.play("Run")
-	else:
-		anim.play("Idle")
+		if is_on_floor() and attacking != 1 and dashing != 1:
+			anim.play("Run")
+
 	#dash mechanism
-	if Input.is_action_just_pressed("ui_dash"):
+	elif Input.is_action_just_pressed("ui_dash"):
+		dashing = 1
 		velocity.x += dash_speed * face
 		anim.play("Dash")
+	#attack mechanism
 
+	elif is_on_floor() and attacking != 1 and dashing != 1:
+		anim.play("Idle")
+	if face < 0:
+		$AnimatedSprite.flip_h = true
+	elif face > 0:
+		$AnimatedSprite.flip_h = false 
 
 	#allows player movements
 	velocity = move_and_slide(velocity, Vector2.UP)
