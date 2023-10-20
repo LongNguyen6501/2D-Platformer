75f42e2d49ac01ace60d8fa2d34216df22db3b17
Huy Long Nguyen <huylongnguyen0605@gmail.com>
2023-10-20 02:04:06 -0700

    applied animation idle + run

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,31 +1,46 @@
 extends KinematicBody2D
 #declare variable
-var speed = 200
-var face = 1
-var jump_speed = -800
+var speed = 300
+var jump_speed = -1000
 var gravity = 4000
 var velocity = Vector2.ZERO
-var dash_speed = 3000
-
+var dash_speed = 6000
+var face = 1
+onready var anim = get_node("AnimationPlayer")
+func _ready():
+	anim.play("Idle")
 func _physics_process(delta):
+	#player gravity code
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+		#jump mechaninism
+	if Input.is_action_just_pressed("ui_jump"):
+		if is_on_floor():
+			velocity.y = jump_speed
+			anim.play("Jump")
+		if velocity.y > 0:
+			anim.play("Fall")
 	#player idle velocity =0
 	velocity.x = 0
 	#go left and right
 	if Input.is_action_pressed("ui_right"):
 		face = 1
 		velocity.x += speed * face
-	if Input.is_action_pressed("ui_left"):
+		anim.play("Run")
+	elif Input.is_action_pressed("ui_left"):
 		face = -1
 		velocity.x += speed * face
+		anim.play("Run")
+	else:
+		anim.play("Idle")
 	#dash mechanism
 	if Input.is_action_just_pressed("ui_dash"):
 		velocity.x += dash_speed * face
-	#player gravity code
-	velocity.y += gravity * delta
+		anim.play("Dash")
+
+
 	#allows player movements
 	velocity = move_and_slide(velocity, Vector2.UP)
-	#jump mechaninism
-	if Input.is_action_just_pressed("ui_jump"):
-		if is_on_floor():
-			velocity.y = jump_speed
+
 	
